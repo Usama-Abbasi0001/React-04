155e16fa0bda93b4e982628d3354af5dfe6ef927
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-05-19 07:12:18 +0500

    react-04

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import StateEffect from './StateEffect';
 
 function App() {
+
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [father, setFather] = useState('')
+  const [adrees, setAdrees] = useState('')
+  const [pass, setPass] = useState('')
+
+  const handleChange = (event) => {
+    setName(event.target.value)
+    console.log(name);
+  }
+  const emailHandleChange = (event) => {
+    setEmail(event.target.value)
+    console.log(email);
+  }
+  const fatherHandleChange = (event) => {
+    setFather(event.target.value)
+    console.log(father);
+  }
+  const passHandleChange = (event) => {
+    setPass(event.target.value)
+    console.log(pass);
+  }
+
+  const adreesHandleChange = (event) => {
+    setAdrees(event.target.value)
+    console.log(adrees);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <legend>CUSTOMER INFO</legend> 
+           NAME:* <input type="name" value={name} onChange={handleChange} placeholder="name.."/>
+           {name}
+        <p>FATHER:* <input type="father" value={father} onChange={fatherHandleChange}  placeholder="farhers.."/></p>
+        {father}
+        <p>ADRESS:* <input type="adress" value={adrees} onChange={adreesHandleChange}   placeholder="adress.."/></p>
+        {adrees}
+        <p>GENDER:*</p> 
+            <input type="radio" name="gender" />Male
+            <input type="radio" name="gender"/>Female
+            <p>PASSWORD:* <input type="password" value={pass} onChange={passHandleChange}   name="password" id="password" placeholder="pass.."/></p>
+            
+           <p> Email:*  <input type="email" name="email" value={email} onChange={emailHandleChange} id="email" placeholder="email.."/></p>
+           {email}
+           <StateEffect />
+           </>
   );
 }
 
